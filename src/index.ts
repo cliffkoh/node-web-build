ff31f78ab7ef0c4a40abc7b19d3f1da02b8aa19a
Cliff Koh <cliffkoh@users.noreply.github.com>
2017-02-04 17:23:04 -0800

    Karma and webpack no longer included by default

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import {
   CopyTask,
   GenerateShrinkwrapTask,
+  IBuildConfig,
   IExecutable,
   ValidateShrinkwrapTask,
   parallel,
@@ -8,21 +9,34 @@ import {
   task,
   watch
 } from '@microsoft/gulp-core-build';
-import { typescript, tslint, text } from '@microsoft/gulp-core-build-typescript';
+import { typescript, tslint } from '@microsoft/gulp-core-build-typescript';
 import { sass } from '@microsoft/gulp-core-build-sass';
-import { karma } from '@microsoft/gulp-core-build-karma';
-import { webpack } from '@microsoft/gulp-core-build-webpack';
 import { serve, reload } from '@microsoft/gulp-core-build-serve';
 import { PostProcessSourceMaps } from './PostProcessSourceMaps';
 import { mocha, instrument } from '@microsoft/gulp-core-build-mocha';
+import { webpack } from '@microsoft/gulp-core-build-webpack';
 
 export * from '@microsoft/gulp-core-build';
 export * from '@microsoft/gulp-core-build-typescript';
 export * from '@microsoft/gulp-core-build-sass';
-export * from '@microsoft/gulp-core-build-karma';
-export * from '@microsoft/gulp-core-build-webpack';
 export * from '@microsoft/gulp-core-build-serve';
 export * from '@microsoft/gulp-core-build-mocha';
+export * from '@microsoft/gulp-core-build-webpack';
+
+export interface IRigConfig {
+  /** (Optional) Task specifying what to run when serving. */
+  serveTask?: IExecutable;
+  /** @deprecated */
+  bundleTask?: IExecutable;
+};
+
+let doNothingTask: IExecutable = {
+  execute: (config: IBuildConfig) => Promise.resolve<void>(),
+  isEnabled: () => false
+}
+
+let rigConfig: IRigConfig = {};
+export const setRigConfig = (newRigConfig: IRigConfig) => { rigConfig = newRigConfig };
 
 // pre copy and post copy allows you to specify a map of dest: [sources] to copy from one place to another.
 export const preCopy: CopyTask = new CopyTask();
@@ -36,12 +50,24 @@ const sourceMatch: string[] = [
   '!src/**/*.scss.ts'
 ];
 
+const serveTask = {
+  execute: (buildConfig: IBuildConfig) => {
+    if (rigConfig.serveTask && rigConfig.serveTask.isEnabled(buildConfig)) {
+      return rigConfig.serveTask.execute(buildConfig);
+    } else if (serve.isEnabled(buildConfig)) {
+      return serve.execute(buildConfig);
+    }
+
+    return Promise.resolve<void>();
+  }
+};
+
 // Define default task groups.
-export const compileTsTasks: IExecutable = parallel(typescript, text);
-export const buildTasks: IExecutable = task('build', serial(preCopy, sass, parallel(compileTsTasks, text), postCopy));
+export const compileTsTasks: IExecutable = typescript;
+export const buildTasks: IExecutable = task('build', serial(preCopy, sass, compileTsTasks, postCopy));
 export const bundleTasks: IExecutable = task('bundle', serial(buildTasks, webpack));
-export const testTasks: IExecutable = serial(sass, compileTsTasks, karma);
-export const defaultTasks: IExecutable = serial(bundleTasks, karma);
+export const testTasks: IExecutable = serial(sass, compileTsTasks, mocha);
+export const defaultTasks: IExecutable = serial(buildTasks, webpack,  mocha);
 export const postProcessSourceMapsTask: PostProcessSourceMaps = new PostProcessSourceMaps();
 export const validateShrinkwrapTask: ValidateShrinkwrapTask = new ValidateShrinkwrapTask();
 export const generateShrinkwrapTask: GenerateShrinkwrapTask = new GenerateShrinkwrapTask();
@@ -57,17 +83,15 @@ task('test-watch', watch(sourceMatch, serial(sass, compileTsTasks, mocha)));
 task('validate-shrinkwrap', validateShrinkwrapTask);
 task('generate', generateShrinkwrapTask);
 
-task('browser-test', serial(sass, testTasks));
-
 // For watch scenarios like serve, make sure to exclude generated files from src (like *.scss.ts.)
 task('serve',
   serial(
-    bundleTasks,
-    serve,
+    buildTasks,
+    serveTask,
     postProcessSourceMapsTask,
     watch(
       sourceMatch, serial(preCopy, sass, compileTsTasks,
-        postCopy, webpack, postProcessSourceMapsTask, reload)
+        postCopy, postProcessSourceMapsTask, reload)
     )
   )
 );
